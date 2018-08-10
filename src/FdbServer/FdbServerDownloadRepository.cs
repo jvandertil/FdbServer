b03c41d2d900a676bb849deb2effd9a6e9daf270
Jos van der Til <jos@vandertil.net>
2018-08-10 15:34:46 +0200

    Update fdbserver-5.2.5 url

--- src/FdbServer/FdbServerDownloadRepository.cs
+++ src/FdbServer/FdbServerDownloadRepository.cs
@@ -9,7 +9,7 @@
         {
             { FdbServerVersion.v5_2_5,
                 new FdbServerUrl(
-                    new Uri(@"https://github.com/jvandertil/FdbServer/releases/download/5.2.5/fdbserver-5.2.5.zip"),
+                    new Uri(@"https://github.com/jvandertil/FdbServer/releases/download/v0.1.0/fdbserver-5.2.5.zip"),
                     "A7CC94FE02FFF018AEAD86AE3C8A6CE08BE9B87C57E391D9BE34F77F93EC2092352CA707D29D6A468FFEB26087A3D6314A65D4D5AAE9F47F7B0D7332577FB660") }
         };
 
