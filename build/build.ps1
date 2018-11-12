b3924986808304b8937f6f37c7e1af16485d7c95
Jos van der Til <jos@vandertil.net>
2018-11-12 20:38:36 +0100

    Update parameters in build script
    Also adds parameters to control test output

--- build/build.ps1
+++ build/build.ps1
@@ -1,3 +1,11 @@
+param (
+    [string]$BuildConfiguration = "Release",
+    [string]$VersionSuffix = "local",
+    [string]$TestResultLogger = "console;verbosity=normal",
+    [string]$TestResultOutputDirectory = "$PSScriptRoot\..\output\TestResults",
+    [string]$OutputDirectory = "$PSScriptRoot\..\output\packages"
+)
+
 # Taken from psake https://github.com/psake/psake
 
 <#
@@ -22,27 +30,18 @@ function Exec
     }
 }
 
-$CI_CONFIG = "$env:CI_CONFIG"
-$CI_VERSION = "$env:CI_VERSION"
-
-if ($CI_CONFIG -eq "")
-{
-    $CI_CONFIG = "Release"
-}
-
 $buildDir = $PSScriptRoot
 $sourceDir = "$PSScriptRoot\.."
-$outputDir = "$PSScriptRoot\..\output"
 
 Remove-Item -Recurse -Force "$sourceDir\src\FdbServer\bin","$sourceDir\src\FdbServer\obj" -ErrorAction SilentlyContinue
-Remove-Item -Force "$outputDir\*.nupkg" -ErrorAction SilentlyContinue
+Remove-Item -Force "$OutputDirectory\*.nupkg" -ErrorAction SilentlyContinue
 
 exec { & dotnet clean $sourceDir\FdbServer.sln }
 
 exec { & dotnet restore --force $sourceDir\FdbServer.sln }
 
-exec { & dotnet build --no-restore -c $CI_CONFIG --version-suffix=$CI_VERSION $sourceDir\FdbServer.sln }
+exec { & dotnet build --no-restore -c $BuildConfiguration --version-suffix=$VersionSuffix $sourceDir\FdbServer.sln }
 
-exec { & dotnet test --no-build -c $CI_CONFIG --verbosity=normal $sourceDir\test\FdbServer.Tests\FdbServer.Tests.csproj }
+exec { & dotnet test --logger=$TestResultLogger -r $TestResultOutputDirectory --no-build -c $BuildConfiguration --verbosity=normal $sourceDir\test\FdbServer.Tests\FdbServer.Tests.csproj }
 
-exec { & dotnet pack -o $outputDir --no-build --include-symbols -c $CI_CONFIG --version-suffix=$CI_VERSION $sourceDir\src\FdbServer\FdbServer.csproj }
+exec { & dotnet pack -o $OutputDirectory --no-build --include-symbols -c $BuildConfiguration --version-suffix=$VersionSuffix $sourceDir\src\FdbServer\FdbServer.csproj }
